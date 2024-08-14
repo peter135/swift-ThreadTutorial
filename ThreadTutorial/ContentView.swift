cf5424a8813840fef7f34a6d6fa588c54b8992b7
wangjian <15995249412@163.com>
2024-08-14 14:38:58 +0800

    Initial Commit

---
+++ ThreadTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadTutorial
+//
+//  Created by 炜季 on 2024/8/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadTutorial/ThreadTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadTutorialApp.swift
+//  ThreadTutorial
+//
+//  Created by 炜季 on 2024/8/14.
+//
+
+import SwiftUI
+
+@main
+struct ThreadTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
